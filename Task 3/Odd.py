0aa7ce17eb26685f57a6c3b153f14c308a1b7f8e
aaryan-manu <139619783+aaryan-manu@users.noreply.github.com>
2023-09-13 22:36:04 +0530

    Add files via upload

---
+++ Task 3/Odd.py
@@ -0,0 +1,14 @@
+def odd_element():
+    z=0
+    x=eval(input("Enter list of numbers: "))
+    for i in x:
+        if i%2!=0:
+            z+=i
+    print(z)
+
+odd_element()
+
+
+
+        
+            
